8cd4142b8982c93f4ee05c304e2ee012b29e8b56
Marcos.Cela <mcela@gradiant.org>
2018-09-17 17:05:32 +0200

    Extract new Date() to a new helper class "CurrentDateProvider"

---
+++ java/print-date/src/main/java/coding/CurrentDateProvider.java
@@ -0,0 +1,9 @@
+package coding;
+
+import java.util.Date;
+
+public class CurrentDateProvider {
+    public Date getCurrentDate() {
+        return new Date();
+    }
+}
--- java/print-date/src/main/java/coding/PrintDate.java
+++ java/print-date/src/main/java/coding/PrintDate.java
@@ -3,7 +3,13 @@ package coding;
 import java.util.Date;
 
 public class PrintDate {
+    private final CurrentDateProvider dateProvider;
+
+    public PrintDate(CurrentDateProvider dateProvider) {
+        this.dateProvider = dateProvider;
+    }
+
     public void printCurrentDate() {
-        System.out.println(new Date());
+        System.out.println(this.dateProvider.getCurrentDate());
     }
 }
