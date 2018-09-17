3c8157fb74cf6a3d29696f26a16ed6f21759246f
Marcos.Cela <mcela@gradiant.org>
2018-09-17 17:07:32 +0200

    Extract printing to console to a new helper class "DatePrinter"

---
+++ java/print-date/src/main/java/coding/DatePrinter.java
@@ -0,0 +1,9 @@
+package coding;
+
+import java.util.Date;
+
+public class DatePrinter {
+    public void printDate(Date currentDate) {
+        System.out.println(currentDate);
+    }
+}
--- java/print-date/src/main/java/coding/PrintDate.java
+++ java/print-date/src/main/java/coding/PrintDate.java
@@ -4,12 +4,16 @@ import java.util.Date;
 
 public class PrintDate {
     private final CurrentDateProvider dateProvider;
+    private final DatePrinter datePrinter;
 
-    public PrintDate(CurrentDateProvider dateProvider) {
+
+    public PrintDate(CurrentDateProvider dateProvider,
+                     DatePrinter datePrinter) {
         this.dateProvider = dateProvider;
+        this.datePrinter = datePrinter;
     }
 
     public void printCurrentDate() {
-        System.out.println(this.dateProvider.getCurrentDate());
+        this.datePrinter.printDate(this.dateProvider.getCurrentDate());
     }
 }
