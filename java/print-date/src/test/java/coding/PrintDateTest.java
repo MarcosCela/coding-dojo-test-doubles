1646b7fa86ed9991a4f3fca1717ab772bad06a17
Marcos.Cela <mcela@gradiant.org>
2018-09-18 08:35:31 +0200

    Ensure that we are really printing something

--- java/print-date/src/test/java/coding/PrintDateTest.java
+++ java/print-date/src/test/java/coding/PrintDateTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.Date;
 
+import static junit.framework.TestCase.assertTrue;
+
 public class PrintDateTest {
     @Test
     public void printDate() throws Exception {
@@ -13,15 +15,21 @@ public class PrintDateTest {
                 return null;
             }
         };
-        DatePrinter testDatePrinter = new DatePrinter() {
-            @Override
-            public void printDate(Date currentDate) {
-            }
-
-        };
+        DatePrinter testDatePrinter = new TestDatePrinter();
 
         PrintDate printDate = new PrintDate(testDateProvider, testDatePrinter);
 
         printDate.printCurrentDate();
+
+        assertTrue(((TestDatePrinter) testDatePrinter).hasPrinted);
+    }
+}
+
+class TestDatePrinter implements DatePrinter {
+    public boolean hasPrinted = false;
+
+    @Override
+    public void printDate(Date currentDate) {
+        hasPrinted = true;
     }
 }
