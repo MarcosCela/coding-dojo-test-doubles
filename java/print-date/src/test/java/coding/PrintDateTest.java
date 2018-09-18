3cc70264108a98609056277afe7fcbd50cf22d6b
Marcos.Cela <mcela@gradiant.org>
2018-09-18 08:39:14 +0200

    Extract testDateProvider to custom interface

--- java/print-date/src/test/java/coding/PrintDateTest.java
+++ java/print-date/src/test/java/coding/PrintDateTest.java
@@ -4,28 +4,34 @@ import org.junit.Test;
 
 import java.util.Date;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class PrintDateTest {
     @Test
     public void printDate() throws Exception {
-        DateProvider testDateProvider = new DateProvider() {
-            @Override
-            public Date getDate() {
-                return null;
-            }
-        };
+        DateProvider testDateProvider = new TestDateProvider();
         DatePrinter testDatePrinter = new TestDatePrinter();
-
         PrintDate printDate = new PrintDate(testDateProvider, testDatePrinter);
 
         printDate.printCurrentDate();
 
         assertTrue(((TestDatePrinter) testDatePrinter).hasPrinted);
+
+    }
+
+}
+
+class TestDateProvider implements DateProvider {
+    public  final Date dateToReturn = new Date(0);
+    @Override
+    public Date getDate() {
+        return dateToReturn;
     }
 }
 
 class TestDatePrinter implements DatePrinter {
+
     public boolean hasPrinted = false;
 
     @Override
